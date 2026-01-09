5961f75d4d3de810fe4bdcba33b78b6c7931602e
PavanPochi06 <pavanpochi06@gmail.com>
2026-01-09 16:18:21 +0530

    delete products when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js";
+import {cart, removeFromCart} from "../data/cart.js";
 import {products} from "../data/products.js";
 import {formatCurrency} from "./utlis/money.js";
 let cartSummaryHTML = "";
@@ -38,7 +38,7 @@ const productId = cartItem.productId;
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -93,3 +93,11 @@ const productId = cartItem.productId;
   `;
 });
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+  }); 
+});
